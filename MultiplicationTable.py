95d7957a16df6bfa4c208c15f6447a64479b5d41
Ashu <141819298+CodeByAshuu@users.noreply.github.com>
2023-09-24 23:27:46 +0530

    Add files via upload

---
+++ MultiplicationTable.py
@@ -0,0 +1,11 @@
+'''
+Made By: SAGAR SAHU
+@Twitter : Ashuuu_7
+@LinkedIn : www.linkedin.com/in/sagarr7
+
+'''
+i=0
+a=int(input("Table of: "))
+while i<10 :
+    i=i+1
+    print(f"{a} * {i} = {a*i}")
